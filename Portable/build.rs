b2f9d115b6216ff312fd50c049c063ca554eab2a
veltrea <veltrea@outlook.com>
2026-06-27 20:29:24 +0900

    Add Portable/build.rs: wire winres icon embedding on Windows

---
+++ Portable/build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // winres は Windows ホストでビルドするときだけ意味を持つ。
+    // macOS / Linux ホストでは何もしない。
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("../icons/vlt-syslogd.ico");
+        res.set("ProductName", "vlt-syslog-portable");
+        res.set("FileDescription", "vlt-syslogd Portable (GUI)");
+        res.set("LegalCopyright", "Copyright (c) 2026 veltrea");
+        res.compile().unwrap();
+    }
+}
